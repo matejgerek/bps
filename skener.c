d2d3087133445f80b89850bd0bbb97a3babd1f21
Matej Gerek <matejgerek@ip-156-141.wifi.tuke.sk>
2023-03-21 09:56:33 +0100

    Remove invisible characters

--- skener.c
+++ skener.c
@@ -1,56 +1,55 @@
-​/* 
-​  Antivirus scanner
-​ */
-​
-​#include <stdio.h>
-​#include <stdlib.h>
-​#include <string.h>
-​#include <sys/types.h>
-​#include <sys/stat.h>
-​#include <sys/wait.h>
-​#include <unistd.h>
-​#include <dirent.h>
-​#include <elf.h>
-​#include <fcntl.h>
-​#include <pwd.h>
-​
-​//infection indicator
-​#define MAGIC 6585
-​#define MAX_BUF 1024
-​
-​static int magic = MAGIC;
-​
-​//virus detection
-​//return 2 if virus found
-​int detect(char *filename, int hd)
-​{
-​    //handle for temp file
-​   int fd;
-​   //file info
-​   struct stat stat;
-​   char *data;
-​   char tmpfile[MAX_BUF];
-​   char cmd[MAX_BUF]="\0";
-​   int tmagic;	  // Store files magic number
-​   int magicloc;  // Location of magic number
-​   Elf32_Ehdr ehdr;
-​
-​
-​/* check for magic(virus identificator) at the end of the file */
-​   if(fstat(hd, &amp;stat) &lt; 0) return 1;
-​   magicloc = stat.st_size - sizeof(magic);
-​   if( lseek(hd, magicloc, SEEK_SET) != magicloc ) return 1;
-​
-​   //load magic character
-​   if(read(hd, &amp;tmagic, sizeof(magic)) != sizeof(magic)) return 1;
-​   //if file is infected, do not infect again
-​   if(tmagic == MAGIC) return 2;
-​   if(lseek(hd, 0, SEEK_SET) != 0) exit(1);
-​
-​
-​   return 0;
-​}
-​
+/* 
+  Antivirus scanner
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <pwd.h>
+
+//infection indicator
+#define MAGIC 6585
+#define MAX_BUF 1024
+
+static int magic = MAGIC;
+//virus detection
+//return 2 if virus found
+int detect(char *filename, int hd)
+{
+    //handle for temp file
+   int fd;
+   //file info
+   struct stat stat;
+   char *data;
+   char tmpfile[MAX_BUF];
+   char cmd[MAX_BUF]="\0";
+   int tmagic;	  // Store files magic number
+   int magicloc;  // Location of magic number
+   Elf32_Ehdr ehdr;
+
+
+/* check for magic(virus identificator) at the end of the file */
+   if(fstat(hd, &amp;stat) &lt; 0) return 1;
+   magicloc = stat.st_size - sizeof(magic);
+   if( lseek(hd, magicloc, SEEK_SET) != magicloc ) return 1;
+
+   //load magic character
+   if(read(hd, &amp;tmagic, sizeof(magic)) != sizeof(magic)) return 1;
+   //if file is infected, do not infect again
+   if(tmagic == MAGIC) return 2;
+   if(lseek(hd, 0, SEEK_SET) != 0) exit(1);
+
+
+   return 0;
+}
+
 //search current directory for executable ELF files
 void scan_dir(char *directory)
 {
@@ -88,17 +87,17 @@ void scan_dir(char *directory)
     }
 }
 
-​
-​
-​int main(int argc, char *argv[], char *envp[])
-​{
-​  
-​   if (argc &lt; 2) {
-​   	printf("Pouzitie %s adresar\n",argv[0]);
-​	exit(1);
-​   }
-​   printf("Prehladavam adresar %s\n",argv[1]); 
-​   //scan directory
-​   scan_dir(argv[1]);
-​   return 0;
-​}
+
+
+int main(int argc, char *argv[], char *envp[])
+{
+  
+   if (argc &lt; 2) {
+   	printf("Pouzitie %s adresar\n",argv[0]);
+	exit(1);
+   }
+   printf("Prehladavam adresar %s\n",argv[1]); 
+   //scan directory
+   scan_dir(argv[1]);
+   return 0;
+}
